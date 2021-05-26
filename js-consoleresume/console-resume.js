449f39b19c0e7122bf63dd059d67450246d7fbaf
sean <tp063366@mail.apu.edu.my>
2021-05-26 17:47:46 +0800

    Updated Lab 3

---
+++ js-consoleresume/console-resume.js
@@ -0,0 +1,7 @@
+let resume = {
+    Name: 'Sean',
+    Career: 'Web Developer, Student',
+    Description: 'I love learning new things.',
+}
+let allCaps = resume.Name.toUpperCase();
+let displayPosition = resume.Career;
